c3651fe7eff054e389af34949da90c77c85ed1dd
Jake Chipps <93165895+jake-chipps@users.noreply.github.com>
2023-05-15 18:57:35 -0600

    Update cash_register_solution.py

--- _docs/lessons/code/cash_register_solution.py
+++ _docs/lessons/code/cash_register_solution.py
@@ -11,7 +11,7 @@ change = float(input("Enter some amount of money: $"))
 
 # Break change into dollars and cents, using only ints
 dollars = int(change)
-cents = int(100 * (change - dollars))
+cents = int(round(100 * (change - dollars)))
 
 # Break down dollars
 
