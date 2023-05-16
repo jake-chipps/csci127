200095fa9a5f37b545a94f9fc3eed7c253cff6ac
Jake Chipps <93165895+jake-chipps@users.noreply.github.com>
2023-05-15 18:41:10 -0600

    Test

---
+++ _docs/lessons/code/cash_register_solution.py
@@ -0,0 +1,60 @@
+# ------------------------------------
+# Joy and Beauty of Data
+# Author: Jake Chipps
+# ------------------------------------
+# Cash Register
+# ------------------------------------
+
+# Get user input for change
+
+change = float(input("Enter some amount of money: $"))
+
+# Break change into dollars and cents, using only ints
+dollars = int(change)
+cents = int(100 * (change - dollars))
+
+# Break down dollars
+
+# Get twenties
+twenties = dollars // 20
+# Get remainder
+dollars = dollars % 20
+
+# Get tens and find remainder
+tens = dollars // 10
+dollars = dollars % 10
+
+# Get fives and find remainder
+fives = dollars // 5
+dollars = dollars % 5
+
+# Get ones
+ones = dollars
+
+# Break down cents
+
+# Get quarters and find remainder
+quarters = cents // 25
+cents = cents % 25
+
+# Get dimes and find remainder
+dimes = cents // 10
+cents = cents % 10
+
+# Get nickels and find remainder
+nickels = cents // 5
+cents = cents % 5
+
+# Get pennies
+pennies = cents
+
+# Print it all out
+print("$", change, "breaks down to: \n",
+      twenties, "$20 bills\n",
+      tens, "$10 bills\n",
+      fives, "$5 bills\n",
+      ones, "$1 bills\n",
+      quarters, "quarters\n",
+      dimes, "dimes\n",
+      nickels, "nickels\n",
+      pennies, "pennies")
